bd1ce7a803635bbd903374b3798f69a8a31f0d02
Armhil Dela Cruz <armhildelacruz12@gmail.com>
2026-06-05 07:14:27 +0800

    fix: bridge express application instance to serverless runtime via vendia

--- api/coach/chat.ts
+++ api/coach/chat.ts
@@ -1,3 +1,5 @@
-import app from "../../server";
+import configureApp from '@vendia/serverless-express';
+import app from '../../server';
 
-export default app;
+// This handles routing all incoming Vercel requests straight into your Express server instance
+export default configureApp({ app });
